a51f4d16fb33b769a651fdce32481736c02e8bc2
Valentina Lilova <valentina.lilova@digital-boss.de>
2022-10-12 17:22:41 +0300

    Edit release script

--- release.sh
+++ release.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 set -e  # exit when any command fails
 npm run build
-git stash push -a dist
-git checkout dist # git checkout --track origin/dist
-git pull
-rm -rf dist
-git stash pop
-npm version --no-commit-hooks --no-git-tag-version $1
-git add --all
-git commit -m "Release v$1"
-git tag "v$1"
+npm version patch
 git push
-git push origin v$1
-git checkout main
